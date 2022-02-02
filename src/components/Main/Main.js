492c70b2584c15de0a5fbe4c2433c02cb5a8cc1b
MalenkovDN <m18dm.dm@gmail.com>
2022-02-02 16:58:54 +0300

    add style main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,7 @@ const Main = () => {
     return (
         <main className="page">
             <div className="page__main_block main_block">
-                <div class="main_block__container _container">
+                <div className="main_block__container _container">
                     <div className="main_block__body">
                         <span className="main_block__text">Hi There</span>
                         <h1 className="main_block__title">I am Malenkov Dmitrii</h1>
