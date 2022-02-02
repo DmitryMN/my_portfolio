fb09404802edc1e5da8874a9828490599958c800
MalenkovDN <m18dm.dm@gmail.com>
2022-02-02 16:38:21 +0300

    add main component

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,14 +7,15 @@ const Main = () => {
             <div className="page__main_block main_block">
                 <div class="main_block__container _container">
                     <div className="main_block__body">
-                        <h1 className="main_block__title">Finance and Consultancy Solution</h1>
-                        <div className="main_block__text">
-                            We know how large objects will act, but things on a small scale.
-                        </div>
+                        <span className="main_block__text">Hi There</span>
+                        <h1 className="main_block__title">I am Malenkov Dmitrii</h1>
+                        <span className="main_block__text">Frontend Developer</span>
                         <div className="main_block__buttons">
-                            <a href="#" className="main_block__button main_block__button_orange">Get Quote Now</a>
                             <a href="#" className="main_block__button main_block__button_border">Learn More</a>
                         </div>
+                    </div>
+                    <div className="main_block__image">
+                        picture block
                     </div>
                 </div>
             </div>
